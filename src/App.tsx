70a047ecc8573a8fe14469015a9ac17cddabbd6e
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-01-27 20:16:22 -0300

    comecando com styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button variant='primary'/>
+      <Button variant='secundary'/>
+      <Button variant='danger'/>
+      <Button variant='success'/>
+      <Button variant='primary'/>
+    </>
   )
 }
 
